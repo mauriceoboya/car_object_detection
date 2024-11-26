5525946af539f9f2216df3e016643c7e0d3090a1
mauriceoboya <mauriq97@gmail.com>
2024-11-26 07:29:08 +0300

    project setup

---
+++ application.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import os
+import logging
+
+FORMAT = '%(asctime)s  %(levelname)-8s %(message)s'
+logging.basicConfig(filename="logging.log",level=logging.INFO,format=FORMAT)
+project_name="CarObjectDetection"
+
+file_paths=[
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/constant/common.py",
+    f"src/{project_name}/config/configurations.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    "main.py",
+    "setup.py",
+    "config/config.yaml",
+    "params.yaml",
+    ".github/workflows/.gitkeep"
+
+]
+
+for file_path in file_paths:
+    file_path=Path(file_path)
+    file_dir,file_name=os.path.split(file_path)
+    if not Path(file_dir).exists():
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Created directory: {file_dir}")
+    else:
+        logging.info(f"Directory already exists: {file_dir}")
+
+    if not file_path.exists():
+        with open(file_path, 'w') as f:
+            pass
+        logging.info(f"Created file: {file_path}")
+    else:
+        logging.info(f"File already exists: {file_path}")
+
